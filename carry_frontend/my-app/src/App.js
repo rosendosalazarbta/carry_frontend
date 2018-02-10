0824ac8b636e1f2928e8258c260c22933429000a
Rosendo Salazar <rosendo.salazar@gestionix.com>
2018-02-10 12:37:45 -0600

    packages

---
+++ carry_frontend/my-app/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App class1 class2" style={styles.container}>
+        <h1>Shopping List for {this.props.name}</h1>
+        <h2>{this.props.text}</h2>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+App.defaultProps = {
+  text: 'default text'
+}
+const styles = {
+  container: {
+    color: "#acacac",
+    fontSize: '15px'
+  }
+}
+
+export default App;
